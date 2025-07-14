a678dd206d74b1a92db0f371c52b07f4f1b08c13
Abderahim <ita27rmp100@gmail.com>
2025-07-14 02:56:32 +0100

    delete all unused folder/files, keep only CountryJSON

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const LastUpdate = require("../countryList.json").LastUpdate
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',{LastUpdate});
-});
-
-module.exports = router;
